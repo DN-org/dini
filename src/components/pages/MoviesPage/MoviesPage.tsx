f9e9dae10021a8136d9ee5032abc330dd53d687e
Dinar <dinarahatovmail.ru@gmail.com>
2024-08-03 00:37:05 +0500

    Сделал каркас на странице MoviesPage

--- src/components/pages/MoviesPage/MoviesPage.tsx
+++ src/components/pages/MoviesPage/MoviesPage.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 
+import { Content } from './MoviesPage.styled';
+
+import Information from './Information/Information';
+
 const MoviesPage: React.FC = () => {
   return (
-    <div>
-      <header>
-        <h1>Welcome to the Home Page</h1>
-      </header>
-      <main>
-        <p>This is the MoviesPage of our application.</p>
-        <p>Feel free to explore and learn more about what we offer.</p>
-      </main>
-      <footer>
-        <p>&copy; 2024 My Website</p>
-      </footer>
-    </div>
+    <Content>
+      <Information/>
+    </Content>
   );
 };
 
---
+++ src/images/SvgMoviesPage.styled.ts
@@ -0,0 +1,18 @@
+import { SvgKinoPoisk, SvgIMDb, SvgFolder } from './SvgMoviesPage';
+import styled from 'styled-components';
+
+export const SvgStarStyled = styled('svg')(() => ({
+  color: 'yellow',
+}));
+
+export const SvgKinoPoiskStyled = styled('svg')(() => ({
+
+}));
+
+export const SvgIMDbStyled = styled('svg')(() => ({
+
+}));
+
+export const SvgFolderStyled = styled('svg')(() => ({
+    
+}));
